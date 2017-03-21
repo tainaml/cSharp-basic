5727ea4e902b261b29cebf5d8ea384a3ef140e2e
tainaml <taina.lima@I00013819>
2017-03-21 18:01:09 -0300

    analise de valores e operacoes

---
+++ AnalisadorDeValores/AnalisadorDeValores/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisadorDeValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, num, soma = 0, divPorCinco = 0, nulo = 0, cont_par = 0;
+            double media;
+
+
+
+            Console.WriteLine("Digite um numero para o limite da condição: ");
+            n = Convert.ToInt16(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Escolha um numero: ");
+                num = Convert.ToInt16(Console.ReadLine());
+
+                soma = soma + num;
+
+                if (num % 5 == 0)
+                {
+                    divPorCinco++;
+                }
+
+                if (num == 0)
+                {
+                    nulo++;
+
+                }
+
+                if (num % 2 == 0)
+                {
+                    cont_par = cont_par + num;
+
+                }
+
+
+
+
+            }
+
+            media = soma / n;
+
+            Console.WriteLine("========================================================");
+
+            Console.WriteLine($"A soma é: {soma}");
+
+            Console.WriteLine($"A media é: {media}");
+
+            Console.WriteLine($"Quantidade de divisíveis por cinco: {divPorCinco} ");
+
+            Console.WriteLine($"Quantidade de numeros nulos: {nulo}");
+
+            Console.WriteLine($"A soma dos números pares é: {cont_par}");
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
